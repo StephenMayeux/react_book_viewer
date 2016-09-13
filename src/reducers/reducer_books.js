09ebf1affdecb3d3443527d41af6da0b1518301f
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-13 17:16:05 -0400

    create books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts'},
+    { title: 'The Cat in the Hat' },
+    { title: 'Coolio Cookbook'}
+  ];
+}
+
+// a Reducer is a function that returns data from global state
