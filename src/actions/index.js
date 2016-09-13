04e2c9f17419c4013ba65596c69f1ebdf509d263
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-13 18:18:54 -0400

    action creator retuns an action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-
+  // this is an action creator
+  // and it needs to return an action, which is just an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
