91981c48c17cc2d331bdd0d9926c7fedda059a5d
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-13 17:09:28 -0400

    reset project

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export function selectBook(book) {
-  // selectBook is an Action creator, needs to return an action (obj with type property)
-  return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  };
-}
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { title: 'JavaScript: The Good Parts', pages: 101},
-    { title: 'The Sund Also Rises', pages: 85},
-    { title: 'Everyone Poops', pages: 942},
-    { title: 'Eloquent JavaScript', pages: 93}
-  ]
-}
