f758533db0885588ef60fe6943c74d9d1aacce67
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-14 09:58:56 -0400

    add pages

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts'},
-    { title: 'The Cat in the Hat' },
-    { title: 'Coolio Cookbook'},
-    { title: 'The Dark Knight Returns' }
+    { title: 'JavaScript: The Good Parts', pages: 341},
+    { title: 'The Cat in the Hat', pages: 29 },
+    { title: 'Coolio Cookbook', pages: 87},
+    { title: 'The Dark Knight Returns', pages: 126 }
   ];
 }
 
